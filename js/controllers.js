5a60ce388e54085065a048bd58e39ce78307afed
Srujana <srujana.vemula@wohlig.com>
2018-04-23 12:48:39 +0530

    removed all swipers and replaced with flexslider for proper view

--- js/controllers.js
+++ js/controllers.js
@@ -274,23 +274,23 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
             }
         });
 
-        $timeout(function () {
-            mySwiper = new Swiper('.swiper-container', {
-                slidesPerView: 3,
-                spaceBetween: 20,
-                slidesPerGroup: 3,
-                loop: true,
-                loopFillGroupWithBlank: false,
-                pagination: {
-                  el: '.swiper-pagination',
-                  clickable: true,
-                },
-                navigation: {
-                  nextEl: '.swiper-button-next',
-                  prevEl: '.swiper-button-prev',
-                },
-            });
-        }, 1000);
+        // $timeout(function () {
+        //     mySwiper = new Swiper('.swiper-container', {
+        //         slidesPerView: 3,
+        //         spaceBetween: 20,
+        //         slidesPerGroup: 3,
+        //         loop: true,
+        //         loopFillGroupWithBlank: false,
+        //         pagination: {
+        //           el: '.swiper-pagination',
+        //           clickable: true,
+        //         },
+        //         navigation: {
+        //           nextEl: '.swiper-button-next',
+        //           prevEl: '.swiper-button-prev',
+        //         },
+        //     });
+        // }, 1000);
 
 
     })
